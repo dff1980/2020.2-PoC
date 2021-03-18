1a636a7dfe65abbc3e1d5bc4eadca04d42314159
dff1980 <pp.zhukov@gmail.com>
2021-03-18 16:05:00 +0300

    Update rmt-mirror.sh

--- scripts/rmt-mirror.sh
+++ scripts/rmt-mirror.sh
@@ -1,9 +1,10 @@
 rmt-cli sync
 
-repos=$(rmt-cli repos list --all)
-for REPO in SLE-Product-SLES15-SP2-{Pool,Updates} SLE-Module-Server-Applications15-SP2-{Pool,Updates} SLE-Module-Basesystem15-SP2-{Pool,Updates} SLE-Module-Containers15-SP2-{Pool,Updates}
- do
-  rmt-cli repos enable $(echo "$repos" | grep "$REPO for sle-15-x86_64" | sed "s/^|\s\+\([0-9]*\)\s\+|.*/\1/");
- done
+#переделать produc вместо repos
+#repos=$(rmt-cli repos list --all)
+#for REPO in SLE-Product-SLES15-SP2-{Pool,Updates} SLE-Module-Server-Applications15-SP2-{Pool,Updates} SLE-Module-Basesystem15-SP2-{Pool,Updates} SLE-Module-Containers15-SP2-{Pool,Updates}
+# do
+#  rmt-cli repos enable $(echo "$repos" | grep "$REPO for sle-15-x86_64" | sed "s/^|\s\+\([0-9]*\)\s\+|.*/\1/");
+# done
 
-rmt-cli mirror 
+#rmt-cli mirror 
